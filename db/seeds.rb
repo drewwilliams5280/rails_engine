328caab7e6a62396208ced7a5540307406777c85
Drew Williams <drewwilliams5280@gmail.com>
2020-10-17 14:51:25 -0600

    WIP customer database, computer starting to freeze

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach("./data/customers.csv", headers: true) do |row|
+  Customer.create!(customer_id: row['id'], first_name: row['first_name'], last_name: row['last_name'], )
+end
