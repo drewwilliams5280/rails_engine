952720a2876e0040dd4786bf6070e2095d21114c
Drew Williams <drewwilliams5280@gmail.com>
2020-10-19 21:23:28 -0600

    Adds invoice model test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,6 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  validates :status, :created_at, :updated_at, presence: true
 end
