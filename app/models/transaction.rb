0fee75ebabd23169c74a9edcdacf1be1f564ca90
Drew Williams <drewwilliams5280@gmail.com>
2020-10-19 21:32:10 -0600

    Adds transaction model test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+
+  validates :credit_card_number, 
+            :credit_card_expiration_date, 
+            :result, 
+            :created_at, 
+            :updated_at, 
+            presence: true
 end
